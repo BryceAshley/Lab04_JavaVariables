ee11edeaf43d057e4f75a700003daed2c26dc108
BryceAshley <110739176+BryceAshley@users.noreply.github.com>
2022-08-06 14:06:08 -0400

    Initial commit - java variables

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int kidsNumber = 5;
+        boolean raining = true;
+        double gasPrice = 7.98;
+        int favNumber = 17;
+        double shoeSize = 21;
+        String birthMonth = "July";
+        String fullName = "Bryce Ashley";
+    }}
